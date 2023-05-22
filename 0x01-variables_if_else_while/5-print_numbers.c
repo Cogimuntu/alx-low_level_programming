2416adb328790025710673fffc3fc4c0b5cbd919
Cogi Muntu <mfredgitonga@gmail.com>
2023-05-22 14:37:01 -0700

    outputiing natural numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if positive or 1 if megative
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d \n", a);
+	}
+}
