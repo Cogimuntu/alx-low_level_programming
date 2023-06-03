1190f600896de86eb7e34752dda01e44cbce0a68
Cogi Muntu <mfredgitonga@gmail.com>
2023-06-03 04:05:17 -0700

    if loop,logical operators,

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (a == b || a > b)
+			{
+				continue;
+			}
+			putchar('0' + a);
+			putchar('0' + b);
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
