200c1e0ace22f7d6552320a2a13de45b0a3eca3f
Cogi Muntu <mfredgitonga@gmail.com>
2023-06-04 02:12:59 -0700

    absoluteness

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - outputs the absolute value of an integer
+ *@g: integer we are finding absolute value of
+ * Return: 1 if successful , 0 otherwise
+ */
+
+int _abs(int g)
+{
+	if (g > 0)
+	{
+		return ('g');
+	}
+	else
+	{
+		g = -g;
+		return ('g');
+	}
+}
