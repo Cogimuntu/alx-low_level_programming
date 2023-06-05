cef2b565339fa2a7d179323154dc493aee76e205
Cogi Muntu <mfredgitonga@gmail.com>
2023-06-05 11:45:33 -0700

    return value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @c: the number
+ * Return: 0 if successful and 1 if otherwise
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
