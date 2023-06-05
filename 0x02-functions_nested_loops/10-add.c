89c5459f8d6f36c97cbb926cb1d6176d43a9922b
Cogi Muntu <mfredgitonga@gmail.com>
2023-06-05 13:19:15 -0700

    some addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @u: the first integer
+ * @m:the second integers to be added
+ * Return: sum of the integers
+ */
+int add(int u, int m)
+{
+	int su;
+
+	su = u + m;
+	return (su);
+}
